09a80b84223140b8686b63a342474d8006e072bc
fred-blondin-curtis <fredekoume33@gmail.com>
2025-03-03 12:39:53 +0100

    Create Ex_02.js

---
+++ Ex_02.js
@@ -0,0 +1,12 @@
+var integer="42"
+var float=42.42
+var string="fourty two"
+var bool=true
+var this_is_null=null
+
+
+console.log(integer)
+console.log(float)
+console.log(string)
+console.log(bool)
+console.log(this_is_null)
